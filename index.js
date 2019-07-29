2cdbfb75c0ec023e67a9a5552e64f746ab8aa80c
Mykola Tymchak <mykolanr@gmail.com>
2019-07-29 22:15:39 +0300

    Added logs and fixed files listing

--- index.js
+++ index.js
@@ -9,17 +9,23 @@ const s3 = new AWS.S3({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-async function listAllObjectsFromS3Bucket(bucket,) {
+async function listAllObjectsFromS3Bucket(bucket, filter) {
     let isTruncated = true;
     let marker;
     let list = [];
+    const regex = new RegExp(filter, 'ig');
     while (isTruncated) {
         let params = {Bucket: bucket};
         try {
             const response = await s3.listObjects(params).promise();
             response.Contents.forEach(item => {
-                console.log(item.Key);
-                list.push(item);
+                if (!filter) {
+                    console.log(item.Key);
+                    list.push(item);
+                } else if(_.get(item, 'Key','').match(regex)){
+                    console.log(item.Key);
+                    list.push(item)
+                }
             });
             isTruncated = response.IsTruncated;
             if (isTruncated) {
@@ -32,26 +38,8 @@ async function listAllObjectsFromS3Bucket(bucket,) {
     return list;
 }
 
-async function listAllBuckets(filter) {
-    const regex = new RegExp(filter, 'ig');
-    s3.listBuckets({}, function (err, data) {
-        let buckets = data.Buckets;
-        let owners = data.Owner;
-        for (let i = 0; i < buckets.length; i += 1) {
-            let bucket = buckets[i];
-            if (_.get(bucket, 'Name', '').match(regex))
-                console.log(bucket.Name);
-        }
-        for (let i = 0; i < owners.length; i += 1) {
-            console.log(owners[i].ID + " " + owners[i].DisplayName);
-        }
-    });
-}
-
 async function clearBucket(bucket, filter) {
-    const regex = new RegExp(filter, 'ig');
-    let items = await listAllObjectsFromS3Bucket(bucket)
-    items = items.filter(el => _.get(el, 'Key', '').match(regex))
+    let items = await listAllObjectsFromS3Bucket(bucket, filter)
 
     for (let i = 0; i < items.length; i += 1) {
         let deleteParams = {Bucket: bucket, Key: items[i].Key};
@@ -70,34 +58,48 @@ async function deleteObject(deleteParams) {
 }
 
 async function uploadLocalFile(myBucket, fileName) {
+    console.log(`Starting upload of file: '${fileName}' to S3 bucket: '${myBucket}'`);
 
+    await new Promise(function (resolve, reject) {
+            s3.createBucket({Bucket: myBucket}, function (err, data) {
+                fs.readFile(fileName, function (err, data) {
+                    if (err) reject(err);
 
-    s3.createBucket({Bucket: myBucket}, function (err, data) {
-
-        fs.readFile(fileName, function (err, data) {
-            if (err) throw err;
-
-            let params = {
-                Key: fileName,
-                Body: data,
-                Bucket: myBucket
-            };
-            s3.upload(params, function (err, data) {
-                if (err) {
-                    console.log('ERROR MSG: ', err);
-                } else {
-                    console.log('Successfully uploaded data');
-                }
+                    let params = {
+                        Key: fileName,
+                        Body: data,
+                        Bucket: myBucket
+                    };
+                    s3.upload(params, function (err, data) {
+                        if (err) {
+                            console.log('ERROR MSG: ', err);
+                        } else {
+                            console.log('Successfully uploaded file');
+                        }
+                        resolve()
+                    });
+                });
             });
-        });
-    });
+        }
+    )
 }
 
-async function runner() {
-    await uploadLocalFile('lcloud-427-ts', 'sample.csv');
-    await listAllBuckets('pg');
-    await listAllObjectsFromS3Bucket('lcloud-427-ts');
-    await clearBucket('lcloud-427-ts', 'sample');
+async function run() {
+    try {
+        console.log('- List all files in an S3 Bucket')
+        await listAllObjectsFromS3Bucket('lcloud-427-ts');
+
+        console.log('\n- Upload a local file to a defined location in the bucket')
+        await uploadLocalFile('lcloud-427-ts', 'sample.csv');
+
+        console.log('\n- List an AWS buckets files that match a "filter" regex')
+        await listAllObjectsFromS3Bucket('lcloud-427-ts', 'sample');
+
+        console.log('\n- Delete all files matching a regex from a bucket');
+        await clearBucket('lcloud-427-ts', 'sample.csv');
+    } catch (err) {
+        console.log("Failure: " + err)
+    }
 }
 
-runner()
+run()
